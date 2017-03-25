88218e4a4065947f647bad4696128eebc989e5b1
Victoria Confeiteiro <victoria.confeiteiro1@marist.edu>
2017-03-25 02:01:20 -0400

    Super Admin Quick Links
    Provides a view of the database and menu options for Super Admin users

---
+++ SuperAdminQuickLinks.php
@@ -0,0 +1,78 @@
+<!--
+Jessica Rieger and Victoria Confeiteiro
+December 4, 2016
+Super Admin Quick Links page
+-->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>Limbo Database</title>
+		<link rel="stylesheet" type="text/css" href="SAstylesheet.css">
+	</head>
+	<body>
+		<img src = "IMG_6150.PNG" alt = "Limbo" height="100" width="130">
+		<hr>
+		<img src = "lost-and-found.png"alt = "Lost and found sign" height="175" width="275" class="centeringIMG">
+		<h1>Limbo Items</h1>
+		<h2> Here is a list of items in our database. To see more information about an item, click it's ID.</h2>
+		<TABLE style="margin: auto;">
+			<TR>
+				<TD><button type="button"><a href = "http://127.0.0.1:8888/SAdminAddStuff.php">Add Stuff</a></button></TD>
+				<TD><button type="button"><a href = "http://127.0.0.1:8888/SuperAdminDeleteStuff.php">Delete Stuff</a></button></TD>
+				<TD><button type="button"><a href = "http://127.0.0.1:8888/SuperAdminUpdateItemInfo.php">Update Stuff</a></button></TD>
+			</TR>
+		</TABLE>
+		<div class="div1">
+			<h5 class="custom"><a href = "http://127.0.0.1:8888/SuperAdminHomepage.php">Back to Super Admin Homepage</a></h5>
+		</div>
+		<hr>
+	</body>
+</html>
+
+<?php
+# Connect to MySQL server and the database
+require( 'includes/connect_db.php' ) ;
+
+# Includes these helper functions
+require( 'includes/helpers.php' ) ;
+
+
+if($_SERVER[ 'REQUEST_METHOD' ] == 'GET') {
+	$column = "";
+	$descr = "";
+	if(isset($_GET['id'])){
+		show_record($dbc, $_GET['id']) ;
+		
+	}
+}
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+			$column = $_POST['column'];
+			$descr = $_POST['descr'];
+	
+			keyword_search($dbc, $column, $descr);		
+}
+
+#Show the records
+show_link_recordsSA($dbc);
+show_form_search($dbc, $column, $descr);
+
+function show_form_search($dbc, $column, $descr){
+	#echo'<p> Hello';
+	echo '<form action ="SuperAdminQuickLinks.php" method="POST" class="centering">';
+		echo '<p class="custom">Select the column you wish to search by:<select name="column">';
+		echo'<option value="finder">Finder</option>';
+			echo'<option value="owner">Owner\'s Name</option>';
+			echo'<option value="Icat">Item Category</option>';
+			echo'<option value="Iname">Item Name</option>';
+			echo'<option value="descr">Item Description</option>';
+			echo'<option value="Ilocation">Location</option>';
+			echo'<option value="Status">Status</option>';
+		echo'</select>';
+				echo '<p class="custom">Enter the keywords you wish to search by:<input type="text" name="descr" value="'. $descr . '"> </p>';
+						
+	echo'<p class="custom"><input type="submit"></p>';
+	echo'</form>';	
+}
+
+?>
